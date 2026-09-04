95080366c5e1527f7696d5b87fc2578e6a826b10
ElioUcan <eliozapata2006@gmail.com>
2026-09-03 22:05:54 -0600

    Last digit activity

---
+++ python_fundamentals/control_flow/last_digit.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+number = __import__('random').randint(-10000, 10000)
+digit = number[-1]
+if digit > 5:
+    print(f"Last digit of {number} is {digit} and is greater than 5")
+elif digit < 6 && digit != 0:
+    print(f"Last digit of {number} is {digit} and is less than 6 and not 0")
+elif digit == 0:
+    print(f"Last digit of {number} is {digit} and is 0")
