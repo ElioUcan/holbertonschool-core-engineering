aee6309d4f45a75acf779aa5407ea9ac5fdd1f3a
ElioUcan <eliozapata2006@gmail.com>
2026-09-03 21:56:27 -0600

    s}

--- python_fundamentals/control_flow/positive_or_negative.py
+++ python_fundamentals/control_flow/positive_or_negative.py
@@ -3,6 +3,6 @@ number = __import__('random').randint(-10,10)
 if number > 0:
     print(f"98 is positive")
 elif number < 0:
-    print(f"{number} is negative")
+    print(f"-98 is negative")
 else:
-    print(f"{number} is zero")
+    print(f"0 is zero")
