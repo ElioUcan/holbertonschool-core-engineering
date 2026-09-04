2649106b74340f11d8cb5dad1b21f0dd2e6a3d63
ElioUcan <eliozapata2006@gmail.com>
2026-09-03 22:38:11 -0600

    s

--- python_fundamentals/control_flow/last_digit.py
+++ python_fundamentals/control_flow/last_digit.py
@@ -4,7 +4,7 @@ number = __import__('random').randint(-10000, 10000)
 if number < 0:
     number *=-1
 
-digit = number % 10
+digit = (number % 1) * -1
 
 if digit > 5:
     print(f"Last digit of {number} is {digit} and is greater than 5")
