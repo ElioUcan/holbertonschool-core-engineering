3b1885b41b52d44fcbba2f42256daf7bb6aa521e
ElioUcan <eliozapata2006@gmail.com>
2026-09-05 16:03:01 -0600

    FINISHED

--- python_fundamentals/control_flow/print_comb3.py
+++ python_fundamentals/control_flow/print_comb3.py
@@ -2,10 +2,8 @@
 
 for i in range(9):
     for j in range(10):
-        if i == 0:
-            print("{:02d}".format(j), end=", ")
-        elif i < 8 and  j != 0: #aqui
-            if i != j and i < j: #Esto elimina el problema de los 11,22,33
+        if i < 8 and  j != 0:
+            if i != j and i < j: #Esto elimina el problema de los 11,21,32,etc.
                 print("{0}{1}".format(i, j), end=", ")
         
 print("{}".format(89))
