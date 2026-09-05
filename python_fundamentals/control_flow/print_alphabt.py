8113325cb4735843f805c010bda1da84bc30a263
ElioUcan <eliozapata2006@gmail.com>
2026-09-04 23:21:11 -0600

    fff

--- python_fundamentals/control_flow/print_alphabt.py
+++ python_fundamentals/control_flow/print_alphabt.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
 letter = "abcdefghijklmnqoprstuvwxyz"
+noletter = ""
 for l in letter:
     if letter != "e" or letter != "q":
-        print("{}".format(l))
+        noletter + l
+print("{}".format(noletter))
 
