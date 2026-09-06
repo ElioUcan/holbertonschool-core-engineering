1677411af1add99f30cd495d8542ed7daa409b9c
ElioUcan <eliozapata2006@gmail.com>
2026-09-05 22:47:13 -0600

    FINISEHED

--- python_fundamentals/functions_modules/calculation.py
+++ python_fundamentals/functions_modules/calculation.py
@@ -4,7 +4,7 @@ from calculator_1 import addition, substraction, multiplication, division
 if __name__ == "__main__":
     a = 10
     b = 5
-    print("{}".format(addition(a, b)))
-    print("{}".format(substraction(a, b))
-    print("{}".format(multiplication(a, b)))
-    print("{}".format(division(a, b)))
+    print("{1} + {2} = {0}".format(addition(a, b), a, b))
+    print("{1} - {2} = {0}".format(substraction(a, b), a, b))
+    print("{1} * {2} = {0}".format(multiplication(a, b), a, b))
+    print("{1} / {2} = {0}".format(division(a, b), a, b))
