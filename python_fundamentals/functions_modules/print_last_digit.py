f01c0f3b1a157661772f5d7f5782504fb47f0256
ElioUcan <eliozapata2006@gmail.com>
2026-09-05 20:31:18 -0600

    FINISHED

--- python_fundamentals/functions_modules/print_last_digit.py
+++ python_fundamentals/functions_modules/print_last_digit.py
@@ -3,5 +3,5 @@ def print_last_digit(number):
     if number < 0:
         number *= -1
     digit = number % 10
-    print("{}".format(digit))
+    print("{}".format(digit), end="")
     return digit
