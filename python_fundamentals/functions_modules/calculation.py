3a6005f2f22e660c3aa7a75b7f1cb52f5c2997ef
ElioUcan <eliozapata2006@gmail.com>
2026-09-05 22:49:05 -0600

    FINISHED

--- python_fundamentals/functions_modules/calculation.py
+++ python_fundamentals/functions_modules/calculation.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
-from calculator_1 import addition, substraction, multiplication, division
+from calculator_1 import add, sub, mul, div
 
 if __name__ == "__main__":
     a = 10
     b = 5
-    print("{1} + {2} = {0}".format(addition(a, b), a, b))
-    print("{1} - {2} = {0}".format(substraction(a, b), a, b))
-    print("{1} * {2} = {0}".format(multiplication(a, b), a, b))
-    print("{1} / {2} = {0}".format(division(a, b), a, b))
+    print("{1} + {2} = {0}".format(add(a, b), a, b))
+    print("{1} - {2} = {0}".format(subs(a, b), a, b))
+    print("{1} * {2} = {0}".format(mul(a, b), a, b))
+    print("{1} / {2} = {0}".format(div(a, b), a, b))
