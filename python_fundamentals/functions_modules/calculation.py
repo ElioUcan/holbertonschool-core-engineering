a526fb5c19b829213a70c68ad99546f372e8c73e
ElioUcan <eliozapata2006@gmail.com>
2026-09-05 22:42:45 -0600

    FINISHED

---
+++ python_fundamentals/functions_modules/calculation.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+from calculator_1 import addition, substraction, multiplication, division
+
+if __name__ == "__main__":
+    a = 10
+    b = 5
+    print("{}".format(addition(a, b)))
+    print("{}".format(substraction(a, b))
+    print("{}".format(multiplication(a, b)))
+    print("{}".format(division(a, b)))
