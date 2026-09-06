6d8688e7713d04b5e3ba7ebcf7895162d4058bb7
ElioUcan <eliozapata2006@gmail.com>
2026-09-05 21:00:29 -0600

    FINISHED

--- python_fundamentals/functions_modules/pow.py
+++ python_fundamentals/functions_modules/pow.py
@@ -1,8 +1,14 @@
 #!/usr/bin/env python3
 def pow(a,b):
     x = 1
+    y = b
     if b == 0:
         return 1
-    for i in range(b):
-        x *= a
+    if b  < 0:
+        y = (b * -1)
+    for i in range(y):
+        if b < 0:
+            x /= a
+        else:
+            x *= a
     return(x)
