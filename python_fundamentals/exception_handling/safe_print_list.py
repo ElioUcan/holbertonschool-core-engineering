6ccb6cc355ba4f70b1e29c9cc17ab6a865284843
ElioUcan <eliozapata2006@gmail.com>
2026-09-07 21:59:24 -0600

    finished

--- python_fundamentals/exception_handling/safe_print_list.py
+++ python_fundamentals/exception_handling/safe_print_list.py
@@ -8,6 +8,7 @@ def safe_print_list(my_list=[], x=0):
         try:
             print("{:d}".format(my_list[i]), end="")
         except Exception as e:
+            print()
             return i
     print()
     return i+1
