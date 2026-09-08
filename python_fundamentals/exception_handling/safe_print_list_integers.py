c303578c4c1ea502acce098a0a5b1db23329196a
ElioUcan <eliozapata2006@gmail.com>
2026-09-07 22:28:12 -0600

    las try for today

--- python_fundamentals/exception_handling/safe_print_list_integers.py
+++ python_fundamentals/exception_handling/safe_print_list_integers.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 
 def safe_print_list_integers(my_list=[], x=0):
-    j = 0
-    for i in range(my_list):
+    i = 0
+    for value in my_list:
         try:
-            print("{:d}".format(my_list[i]), end="")
-            j+=1
+            print("{:d}".format(value), end="")
+            i+=1
         except Exception as e:
             continue
     print()
-    return j
+    return i
