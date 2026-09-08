7147f74732be42d3f872203b9a951a19fb32c270
ElioUcan <eliozapata2006@gmail.com>
2026-09-07 22:24:51 -0600

    finished

--- python_fundamentals/exception_handling/safe_print_list_integers.py
+++ python_fundamentals/exception_handling/safe_print_list_integers.py
@@ -2,11 +2,11 @@
 
 def safe_print_list_integers(my_list=[], x=0):
     i = 0
-    for value in range(len(my_list)):
+    for value in range(my_list):
         try:
             print("{:d}".format(value), end="")
             i++
-        except valueError as e:
+        except Exception as e:
             continue
     print()
     return i
