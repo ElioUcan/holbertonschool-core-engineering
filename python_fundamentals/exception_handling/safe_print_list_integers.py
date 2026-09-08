18e6268bec19ca8d74997207764211f4c3111651
ElioUcan <eliozapata2006@gmail.com>
2026-09-07 22:24:07 -0600

    finished

--- python_fundamentals/exception_handling/safe_print_list_integers.py
+++ python_fundamentals/exception_handling/safe_print_list_integers.py
@@ -7,6 +7,6 @@ def safe_print_list_integers(my_list=[], x=0):
             print("{:d}".format(value), end="")
             i++
         except valueError as e:
-            pass
+            continue
     print()
     return i
