c96ad9b038458837009ca9d7f5eab100c660ca77
ElioUcan <eliozapata2006@gmail.com>
2026-09-07 21:38:22 -0600

    finished

--- python_fundamentals/exception_handling/safe_print_list.py
+++ python_fundamentals/exception_handling/safe_print_list.py
@@ -2,7 +2,8 @@
 
 def safe_print_list(my_list=[], x=0):
     if x == 0:
-        return x 
+        print(x)
+        return x
     for i in range(x):
         try:
             print(my_list[i], end="")
