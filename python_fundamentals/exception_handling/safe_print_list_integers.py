b2fc0a57d74d4ae217993b925dee5e1542598eab
ElioUcan <eliozapata2006@gmail.com>
2026-09-08 19:49:12 -0600

    finished

--- python_fundamentals/exception_handling/safe_print_list_integers.py
+++ python_fundamentals/exception_handling/safe_print_list_integers.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 
 def safe_print_list_integers(my_list=[], x=0):
+    # lo que falta es solo condicional para que el array no imprima 
+    # mas que el valor de X y ya coneso estaria 
     i = 0
-    for value in my_list:
+    for j in range(x):
         try:
-            print("{:d}".format(value), end="")
+            print("{:d}".format(my_list[j]), end="")
             i+=1
         except Exception as e:
             continue
