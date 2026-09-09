5564590696713d41567448de7f0f5a5f4c4ae5ad
ElioUcan <eliozapata2006@gmail.com>
2026-09-08 20:04:06 -0600

    finishged

--- python_fundamentals/exception_handling/safe_print_division.py
+++ python_fundamentals/exception_handling/safe_print_division.py
@@ -5,7 +5,8 @@ def safe_print_division(a, b):
         result =  a / b 
     except Exception as e:
         print("Inside result: None")
+        print("{0} / {1} = None".format(a, b))
         return None
     finally:
-        print("Inside result: {}".format(result))
+        print("{0} / {1} = {2}".format(a, b, result))
         return result
