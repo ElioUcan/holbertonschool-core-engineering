51a0f456c8815b6f317565e635c231025227af92
ElioUcan <eliozapata2006@gmail.com>
2026-09-12 14:27:45 -0600

    Finished

--- python_oop/classes_and_object_model/4-square.py
+++ python_oop/classes_and_object_model/4-square.py
@@ -6,10 +6,6 @@
 class Square:
     """Does nothing"""
     def __init__(self, size=0):
-        if type(size) is not int:
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
         self.__size = size
 
     @property
@@ -19,6 +15,10 @@ class Square:
 
     @size.setter
     def size(self, value):
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
         self.__size = value
     
     def area(self):
