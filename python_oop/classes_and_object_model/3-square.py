1009e4041471e84cd4da3cdde478d7500802b51d
ElioUcan <eliozapata2006@gmail.com>
2026-09-12 14:04:18 -0600

    Finished

--- python_oop/classes_and_object_model/3-square.py
+++ python_oop/classes_and_object_model/3-square.py
@@ -5,12 +5,11 @@
 class Square:
     """Does nothing"""
 
-    def __init__(self, size=0, side = 0):
+    def __init__(self, size=0):
         if type(size) is not int:
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
-        self.__side = side
     def area(self):
-        return self.__size * self.__side
+        return self.__size * self.__size
