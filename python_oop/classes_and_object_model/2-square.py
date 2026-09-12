71fa639199b1d76766005c4abf69648474f52269
ElioUcan <eliozapata2006@gmail.com>
2026-09-12 13:50:31 -0600

    Finished

--- python_oop/classes_and_object_model/2-square.py
+++ python_oop/classes_and_object_model/2-square.py
@@ -5,8 +5,9 @@
 
 class Square:
     """Does nothing"""
-    def __init__(self, size = 0):
-        if type(size) != int:
+
+    def __init__(self, size=0):
+        if type(size) is not int:
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
