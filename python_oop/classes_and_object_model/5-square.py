a86487fbfd033cc396f9f274e0c2712524e3c577
ElioUcan <eliozapata2006@gmail.com>
2026-09-12 14:35:58 -0600

    Finished

---
+++ python_oop/classes_and_object_model/5-square.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+"""Class that does nothing"""
+
+
+class Square:
+    """Does nothing"""
+    def __init__(self, size=0):
+        self.__size = size
+
+    @property
+    def size(self):
+        """The  property size"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        return self.__size * self.__size
+
+    def my_print(self):
+        for i in range(self.__size):
+            for j in range(i)
+                print("#")
