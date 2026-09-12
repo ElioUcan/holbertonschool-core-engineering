37934ecc404b43689267f059def981611dd7a1cc
ElioUcan <eliozapata2006@gmail.com>
2026-09-12 14:43:12 -0600

    Finished

--- python_oop/classes_and_object_model/5-square.py
+++ python_oop/classes_and_object_model/5-square.py
@@ -25,5 +25,6 @@ class Square:
         return self.__size * self.__size
 
     def my_print(self):
+        if self.__size > 0:
         for i in range(self.__size):
             print("#" * self.__size)
