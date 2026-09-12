446f21166b202e754b21b54fb33bdaef846076d5
ElioUcan <eliozapata2006@gmail.com>
2026-09-12 14:55:22 -0600

    Finished

--- python_oop/classes_and_object_model/6-square.py
+++ python_oop/classes_and_object_model/6-square.py
@@ -6,22 +6,22 @@
 
 class Square:
         """Represents a square."""
-    
+
         def __init__(self, size=0, position=(0, 0)):
             """Initialize a new Square.
-    
+
             Args:
                 size (int): The size of the new square.
                 position (tuple): The position of the new square.
             """
             self.size = size
             self.position = position
-    
+
         @property
         def size(self):
             """Get or set the current size of the square."""
             return self.__size
-    
+
         @size.setter
         def size(self, value):
             if not isinstance(value, int):
@@ -29,19 +29,19 @@ class Square:
             if value < 0:
                 raise ValueError("size must be >= 0")
             self.__size = value
-    
+
         @property
         def position(self):
             """Get or set the current position of the square."""
             return self.__position
-    
+
         @position.setter
         def position(self, value):
             if (not isinstance(value, tuple) or
                     len(value) != 2 or
                     not all(isinstance(num, int) for num in value) or
                     not all(num >= 0 for num in value)):
-                raise TypeError("position must be a tuple of 2 positive integers")
+                raise TypeError("position must be a tuple of 2")
             self.__position = value
 
         def area(self):
