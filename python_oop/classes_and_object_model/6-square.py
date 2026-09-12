f0cae63e8a9eecc1dba9ed4903a1eaef057c68ea
ElioUcan <eliozapata2006@gmail.com>
2026-09-12 14:54:02 -0600

    Finished

--- python_oop/classes_and_object_model/6-square.py
+++ python_oop/classes_and_object_model/6-square.py
@@ -1,18 +1,25 @@
-#!/usr/bin/env python3    
-"""Module that defines a Square class."""    
-class Square:
-    """Represents a square."""
-
-    def __init__(self, size=0, position=(0, 0)):
-        """Initialize a new Square."""
-        self.size = size
-        self.position = position
-
+    #!/usr/bin/env python3
+    """Module that defines a Square class."""
+    
+    
+    class Square:
+        """Represents a square."""
+    
+        def __init__(self, size=0, position=(0, 0)):
+            """Initialize a new Square.
+    
+            Args:
+                size (int): The size of the new square.
+                position (tuple): The position of the new square.
+            """
+            self.size = size
+            self.position = position
+    
         @property
         def size(self):
             """Get or set the current size of the square."""
             return self.__size
-
+    
         @size.setter
         def size(self, value):
             if not isinstance(value, int):
@@ -20,12 +27,12 @@ class Square:
             if value < 0:
                 raise ValueError("size must be >= 0")
             self.__size = value
-
+    
         @property
         def position(self):
             """Get or set the current position of the square."""
             return self.__position
-
+    
         @position.setter
         def position(self, value):
             if (not isinstance(value, tuple) or
