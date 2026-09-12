b6d248858476a3fb23a4c7f937f7125d919ad13a
ElioUcan <eliozapata2006@gmail.com>
2026-09-12 14:18:45 -0600

    Finished

--- python_oop/classes_and_object_model/3-square.py
+++ python_oop/classes_and_object_model/3-square.py
@@ -11,6 +11,14 @@ class Square:
         elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
+    @property
+    def size(self):
+        """The  property size"""
+        return self.__size
 
+    @.setter
+    def (self, value):
+        self.__size = value
+    
     def area(self):
         return self.__size * self.__size
