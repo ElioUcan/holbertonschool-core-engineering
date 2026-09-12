49fb0578fce9baf038bba1ae668a0db18ae08f1d
ElioUcan <eliozapata2006@gmail.com>
2026-09-12 14:39:42 -0600

    Finished

--- python_oop/classes_and_object_model/5-square.py
+++ python_oop/classes_and_object_model/5-square.py
@@ -26,5 +26,5 @@ class Square:
 
     def my_print(self):
         for i in range(self.__size):
-            for j in range(self.__size)
+            for j in range(self.__size):
                 print("#")
