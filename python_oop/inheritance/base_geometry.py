585b3adae07a760df8d008c24998cf03e9b4d9b0
ElioUcan <eliozapata2006@gmail.com>
2026-09-13 12:02:38 -0600

    Finished

--- python_oop/inheritance/base_geometry.py
+++ python_oop/inheritance/base_geometry.py
@@ -4,9 +4,12 @@
 """Base geometry class template made for other classes"""
 
 class BaseGeometry:
+    """Raises an error if the area method is not implemented"""
 
     def area(self):
         raise Exception("area() is not implemented")
+
+    """Checks for the value to be an int or greater than 0"""
 
     def integer_validator(self,name,value):
         if type(value) is not int:
