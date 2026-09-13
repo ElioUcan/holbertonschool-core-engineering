b8697168fa2298f0ddea2b044011eedba10a274e
ElioUcan <eliozapata2006@gmail.com>
2026-09-13 12:28:43 -0600

    Finished

--- python_oop/inheritance/1-rectangle.py
+++ python_oop/inheritance/1-rectangle.py
@@ -23,7 +23,7 @@ class Rectangle(BaseGeometry):
     """Init of the class"""
 
     def __init__(self, width, height):
-        self.integer_validator(width)
-        self.integer_validator(height)
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
         self._width = width
         self._height = height
