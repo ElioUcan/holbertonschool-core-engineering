d7c637e250a126afc76d77ff7397f074379c2c5b
ElioUcan <eliozapata2006@gmail.com>
2026-09-13 12:53:32 -0600

    Finished

---
+++ python_oop/inheritance/2-rectangle.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+"""Rectangle class"""
+
+BaseGeometry = __import__("base_geometry").BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """Init of the class"""
+
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self._width = width
+        self._height = height
+
+    def area(self):
+        return self._width * self._height
+
+    def __str__(self):
+        return  f"""[Rectangle] {self._width}/{self._height}"""
