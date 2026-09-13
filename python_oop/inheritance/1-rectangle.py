63fb2299415bc682ff0399ef05b757b5c761092c
ElioUcan <eliozapata2006@gmail.com>
2026-09-13 12:25:51 -0600

    Finished

--- python_oop/inheritance/1-rectangle.py
+++ python_oop/inheritance/1-rectangle.py
@@ -1,30 +1,17 @@
 #!/usr/bin/env python3
 
-"""Base geometry class template made for other classes"""
+from base_geometry import BaseGeometry
 
-
-class BaseGeometry:
-
-    """Raises an error if the area method is not implemented"""
-    def area(self):
-        raise Exception("area() is not implemented")
-
-    """Checks for the value to be an int or greater than 0"""
-
-    def integer_validator(self, name, value):
-        if type(value) is not int:
-            raise TypeError("{} must be an integer".format(name))
-        elif value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
 
 """Rectangle class"""
+
 
 class Rectangle(BaseGeometry):
     
     """Init of the class"""
 
     def __init__(self, width, height):
-        integer_validator(width)
-        integer_validator(height)
+        self.integer_validator(width)
+        self.integer_validator(height)
         self._width = width
-        self.height = height
+        self._height = height
--- python_oop/inheritance/base_geometry.py
+++ python_oop/inheritance/base_geometry.py
@@ -17,3 +17,5 @@ class BaseGeometry:
             raise TypeError("{} must be an integer".format(name))
         elif value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
+if __name__ == "__main__":
+    base = BaseGeometry()
