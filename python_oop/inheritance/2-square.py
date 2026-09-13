6baf32aa6f1e5a14d310a70ef95b952e6f0cf456
ElioUcan <eliozapata2006@gmail.com>
2026-09-13 15:22:02 -0600

    Finished

---
+++ python_oop/inheritance/2-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+"""Class of a square"""
+
+Rectangle = __import__("2-rectangle").Rectangle
+
+
+class Square(Rectangle):
+    """Class of a square"""
+
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        self._size = size
+
+    def area(self):
+        return self._size**2
+
+    def __str__(self):
+        return f"""[Square] {self._size}/{self._size}"""
