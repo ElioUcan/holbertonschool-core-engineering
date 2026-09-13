4fba8e9243165cf84b5d52e9e103ad6fa8647660
ElioUcan <eliozapata2006@gmail.com>
2026-09-13 15:18:47 -0600

    Finished

--- python_oop/inheritance/1-square.py
+++ python_oop/inheritance/1-square.py
@@ -5,7 +5,9 @@
 Rectangle = __import__("2-rectangle").Rectangle
 
 class square(Rectangle):
+    
     """Class of a square"""
+
     def __init__(self, size):
         self.integer_validator("size", size)
         self._size = size
