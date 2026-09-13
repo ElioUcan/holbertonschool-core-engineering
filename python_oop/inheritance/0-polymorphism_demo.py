4bc83088fbf675caa84dc4bd28352d62c226eba3
ElioUcan <eliozapata2006@gmail.com>
2026-09-13 11:41:05 -0600

    Finished

---
+++ python_oop/inheritance/0-polymorphism_demo.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+class Animal:
+    def speak(self):
+        return "Some sound"
+
+class Dog(Animal):
+    def speak(self):
+        return "Woof"
+
+class Cat(Animal):
+    def speak(self):
+        return "Meow"
+
+
+dog = Dog():
+    print(isintance(dog,Dog))
+    print(isintance(dog,Animal))
+    print(issubclass(Dog, Animal))
