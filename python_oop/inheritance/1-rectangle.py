5b8907fa7a1bd846400870546efa9ac20d7a0393
ElioUcan <eliozapata2006@gmail.com>
2026-09-13 12:27:41 -0600

    Finished

--- python_oop/inheritance/1-rectangle.py
+++ python_oop/inheritance/1-rectangle.py
@@ -1,10 +1,22 @@
 #!/usr/bin/env python3
 
-from base_geometry import BaseGeometry
+"""Base geometry class template made for other classes"""
 
+class BaseGeometry:
+
+    """Raises an error if the area method is not implemented"""
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    """Checks for the value to be an int or greater than 0"""
+
+    def integer_validator(self, name, value):
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        elif value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
 
 """Rectangle class"""
-
 
 class Rectangle(BaseGeometry):
     
