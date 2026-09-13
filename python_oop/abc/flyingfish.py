a8b1f5bd43e9c5eb1c8613d967f16048a8604fbb
ElioUcan <eliozapata2006@gmail.com>
2026-09-13 17:52:45 -0600

    Finished

--- python_oop/abc/flyingfish.py
+++ python_oop/abc/flyingfish.py
@@ -27,9 +27,9 @@ class FlyingFish(Bird, Fish):
     """The flying fish class"""
 
     def fly(self):
-        print("The flying fish is soaring")
+        print("The flying fish is soaring!")
     def swim(self):
-        print("The flying fish is swimming")
+        print("The flying fish is swimming!")
     def habitat(self):
         print("The flying fish lives both in water and the sky!")
 
