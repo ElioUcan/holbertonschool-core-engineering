121f7b97f611e053dee0e1daf36fe480fbb2dcde
ElioUcan <eliozapata2006@gmail.com>
2026-09-13 20:49:15 -0600

    Finished

---
+++ python_oop/abc/verboselist.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+class Verboselist(list):
+
+    """A list that does not stop talking"""
+
+    def append(self, item):
+        super().append(item)
+        print(f"Added {item} to list")
+
+    def extend(self, x):
+        super().extend(x)
+        print(f"Extended the list with {x} items")
+    def remove(self, item):
+        if item in list:
+            super().remove(item)
+            print(f"Removed {item} fron the list")
+
+    def pop(self, item):
+        super.pop(item)
+        print(f"Popped {item} from the list")
