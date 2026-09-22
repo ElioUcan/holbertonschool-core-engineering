27f2d0f1e9ca4269b6ae6db67eaa389713925a30
ElioUcan <eliozapata2006@gmail.com>
2026-09-21 18:59:38 -0600

    Finished

---
+++ python_advanced/file_handling/append_write.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+"""
+append file
+
+"""
+
+
+def append_write(filename="", text=""):
+    """Function that appends and returns the number of appended chars"""
+
+    i = 0
+    with open(filename, mode=a, encode="utf-8") as f:
+        for char in text:
+            i += 1
+        f.write(text)
