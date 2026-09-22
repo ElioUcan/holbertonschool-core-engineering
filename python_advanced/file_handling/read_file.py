7fa26a0700049965c0ff6098bd1cb4c1c88118d7
ElioUcan <eliozapata2006@gmail.com>
2026-09-21 18:51:22 -0600

    Finished

--- python_advanced/file_handling/read_file.py
+++ python_advanced/file_handling/read_file.py
@@ -8,5 +8,9 @@ File opening handeling only read option
 def read_file(filename=""):
     """Reads a file from a given path"""
 
+    i = 0
     with open(filename, mode="r", encoding="utf-8") as file:
-        print(file.read(), end="")
+        for line in file:
+            for char in line:
+                i += 1
+        return i
---
+++ python_advanced/file_handling/write_file.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+File opening handeling write option
+
+"""
+
+
+def write_file(filename="", text=""):
+    """Writes something a file from a given path"""
+
+    i = 0
+    with open(filename, mode="w", encoding="utf-8") as file:
+        for char in text:
+            i += 1
+        file.write(text)
+        return i
+
+
