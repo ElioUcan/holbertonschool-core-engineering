7f7e496a46d0aa06de1a44beec24be21435a0310
ElioUcan <eliozapata2006@gmail.com>
2026-09-21 18:46:20 -0600

    Finished

--- python_advanced/file_handling/read_file.py
+++ python_advanced/file_handling/read_file.py
@@ -9,4 +9,4 @@ def read_file(filename=""):
     """Reads a file from a given path"""
 
     with open(filename, mode="r", encoding="utf-8") as file:
-        print(file.read())
+        print(file.read(), end="")
